ea3189f4949addb63a69a2e411561fdd2ac72ee5
brunardo19 <balderramab19@gmail.com>
2025-09-04 18:57:56 -0300

    Agregados Hashmaps en hiloControlador

--- src/main/java/org/example/HiloControlador.java
+++ src/main/java/org/example/HiloControlador.java
@@ -1,12 +1,12 @@
 package org.example;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class HiloControlador extends Thread {
     ConcurrentHashMap<String, Object> estado;
-    private double[] humedadArray;
+    private ConcurrentHashMap<String, Double> humedadades;
+    private ConcurrentHashMap<String, Boolean> electrovalvulas;
     private double temperatura;
     private double radiacion;
     private boolean lluvia;
@@ -16,11 +16,16 @@ public class HiloControlador extends Thread {
         this.temperatura = 0.0;
         this.radiacion = 0.0;
         this.lluvia = false;
-        this.humedadArray = new double[5];
+        for (int i = 0; i < 5; i++) {
+            this.humedadades.put(String.valueOf(i),0.0);
+        }
+        for (int i = 0; i < 7; i++) {
+            this.electrovalvulas.put(String.valueOf(i),false);
+        }
         this.estado.put("temperatura", 0.0);
         this.estado.put("radiacion", 0.0);
         this.estado.put("lluvia", false);
-        this.estado.put("humedadArray", this.humedadArray);
+        this.estado.put("humedadArray", this.humedadades);
     }
 
     public void run() {
@@ -29,6 +34,14 @@ public class HiloControlador extends Thread {
                 this.temperatura = (double) this.estado.get("temperatura");
                 this.radiacion = (double) this.estado.get("radiacion");
                 this.lluvia = (boolean) this.estado.get("lluvia");
+                this.electrovalvulas = (ConcurrentHashMap<String, Boolean>) this.estado.get("electrovalvulas");
+                this.humedadades = (ConcurrentHashMap<String, Double>) this.estado.get("humedadades");
+
+                //Calcular INR
+
+
+                //Activar electrovalvulas
+
                 System.out.printf("  Temperatura : %.2f °C%n", this.temperatura);
                 System.out.printf("  Radiación   : %.2f W/m²%n", this.radiacion);
                 System.out.printf("  Lloviendo   : %s%n", this.lluvia ? "Sí" : "No");
